40b5d48a46691c2d21eab377efd40bee71e2dcf6
Rajasekhar <rajalekkalaa@gmail.com>
2021-08-15 23:14:54 +0800

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom"
+export default function App() {
+    return(
+        <Router>
+            <div className="container">
+                <div className="row">
+                    <h1 className="mt-3">Go, Watch A Movie!</h1>
+                    <hr className="mb-3" />
+                </div>
+                <div className="row">
+                    <div className="col-md-2">
+                        <nav>
+                            <ul className="list-group">
+                                <li className="list-group-item">
+                                    <Link to="/"> Home </Link>
+                                </li>
+                                <li className="list-group-item">
+                                    <Link to="/movies"> Movies </Link>
+                                </li>
+                                <li className="list-group-item">
+                                    <Link to="/admin"> Movies Catalogue </Link>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div className="col-md-10">
+                        <Switch>
+                            <Route path="/movies">
+                                <Movies />
+                            </Route>
+                            <Route path="/admin">
+                                <Admin />
+                            </Route>
+                            <Route path="/">
+                                <Home />
+                            </Route>
+                        </Switch>
+                    </div>
+                </div>
+            </div>
+        </Router>
+    )
 }
 
-export default App;
+function Home() {
+    return(
+        <h4>Home</h4>
+    )
+}
+
+function Movies() {
+    return(
+        <h4>Movies</h4>
+    )
+}
+
+function Admin() {
+    return(
+        <h4>Admin</h4>
+    )
+}
